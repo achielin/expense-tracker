0d5734b116ff21086ebff7e100d09636a307b771
achielin <achielinder@gmail.com>
2026-03-23 10:46:33 +0300

    middleware

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt')
 require('dotenv').config()
 
+const authenticate = require('./middleware/auth')
+
 
 const app = express()
 app.use(express.json())
